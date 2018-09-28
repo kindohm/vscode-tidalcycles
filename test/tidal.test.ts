1377299fd7467fcc314f7978b68d2776de46f96f
Ross Gardiner <ross-public@gmx.com>
2018-09-29 00:12:55 +0200

    Add multiline Tidal test

--- test/tidal.test.ts
+++ test/tidal.test.ts
@@ -5,7 +5,7 @@ import { Logger } from '../src/logging';
 import * as TypeMoq from "typemoq";
 
 suite("Tidal", () => {
-    test("Single line sent to tidal is passed to GHCi", () => {
+    test("Single line sent to Tidal is passed to GHCi", () => {
         let mockedLogger = TypeMoq.Mock.ofType<Logger>();
         let mockedConfig = TypeMoq.Mock.ofType<Config>();
         let mockedGhci = TypeMoq.Mock.ofType<Ghci>();
@@ -17,6 +17,23 @@ suite("Tidal", () => {
         mockedGhci.setup(ghci => ghci.writeLn(':}')).verifiable(TypeMoq.Times.once());
 
         return tidal.sendTidalExpression('d1 $ sound "bd"').then(() => {
+            mockedGhci.verifyAll();
+        });
+    });
+
+    test("Multiple lines sent to Tidal are passed to GHCi", () => {
+        let mockedLogger = TypeMoq.Mock.ofType<Logger>();
+        let mockedConfig = TypeMoq.Mock.ofType<Config>();
+        let mockedGhci = TypeMoq.Mock.ofType<Ghci>();
+        let tidal: Tidal = new Tidal(mockedLogger.object, mockedConfig.object, mockedGhci.object);
+        tidal.tidalBooted = true;
+
+        mockedGhci.setup(ghci => ghci.writeLn(':{')).verifiable(TypeMoq.Times.once());
+        mockedGhci.setup(ghci => ghci.writeLn('d1 $')).verifiable(TypeMoq.Times.once());
+        mockedGhci.setup(ghci => ghci.writeLn('sound "bd"')).verifiable(TypeMoq.Times.once());
+        mockedGhci.setup(ghci => ghci.writeLn(':}')).verifiable(TypeMoq.Times.once());
+
+        return tidal.sendTidalExpression('d1 $\r\nsound "bd"').then(() => {
             mockedGhci.verifyAll();
         });
     });
